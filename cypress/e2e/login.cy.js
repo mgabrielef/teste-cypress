62a90a4516b51c902ffcb497e09caf4c1fc89067
Gabriele Ferreira <mariagabrieleoliveira@gmail.com>
2023-03-11 04:25:34 -0300

    testes de login adicionados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,10 +7,26 @@ context('Login', () => {
   })
 
   it('Should login successfully', () => {
-    cy.get('#username')
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('a > .hidden-xs').should('contain', 'Welcome')
 
   })
-  //it('Should show error message if user or password is invalid', () => {
-      
-  //})
+  it('Should show error message if user is invalid', () => {
+    cy.get('#username').type('aluno@teste')
+    cy.get('#password').type('teste@teste.com')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-error').should('contain', 'Erro: o usuário aluno@teste não está cadastrado neste site')
+  })
+
+  it('Should show error message if password is invalid', () => {
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@test')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta')
+  })
 })
